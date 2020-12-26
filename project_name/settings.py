22d88bac16d430f58d1c5432e8847c76f7439ee2
Louis Agyapong <agyapong.louis@gmail.com>
2020-12-26 10:08:02 +0000

    updated settings file

--- project_name/settings.py
+++ project_name/settings.py
@@ -38,12 +38,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # External apps that go before Django's
+
+    # Django modules
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local apps
+
+    # External apps
 ]
 
 MIDDLEWARE = [
