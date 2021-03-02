41589f05b250871dc6d1827197ec46227da0de5a
Louis Agyapong <agyapong.louis@gmail.com>
2021-03-02 22:55:43 +0000

    minor updates

--- project_name/urls.py
+++ project_name/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -24,5 +24,8 @@ urlpatterns = [
 
 
 if settings.DEBUG:
+    import debug_toolbar
+
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += (path("__debug__/", include(debug_toolbar.urls)),)
